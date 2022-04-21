5f2187058881767b67c908e2ff4f08fadc628fe5
Laura <lauradfenn@gmail.com>
2022-04-21 20:28:21 +0100

    js: added temp, city, description, humidity and wind elements

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
 let apiKey = "2f6c0570b02743f6b440bec23aa5a698";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&lat={lat}&lon={lon}&appid=${apiKey}&units=metric`;
 
