6ed133b04ac7e0e449ec0b895c2d53f147245e11
Laura <lauradfenn@gmail.com>
2022-04-22 19:49:42 +0100

    Added form and search city functions

--- src/app.js
+++ src/app.js
@@ -43,7 +43,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "2f6c0570b02743f6b440bec23aa5a698";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=california&lat={lat}&lon={lon}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "2f6c0570b02743f6b440bec23aa5a698";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lat={lat}&lon={lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("London");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
